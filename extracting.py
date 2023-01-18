e7a95c29f5f7c6ac4b3c35ffff38645c262b903b
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-01-18 19:26:02 +0100

    Completed extracting.py

--- extracting.py
+++ extracting.py
@@ -3,22 +3,44 @@ import os.path as osp
 import os
 from tqdm import tqdm
 from collections import Counter
+import pickle
 
-ARCHIVE_PATH = osp.expanduser("~/store/d3m/datasets-master.tar.bz2.1")
+ARCHIVE_PATH = osp.expanduser("~/store/d3m/datasets-master.tar.bz2")
 
 tfile = tarfile.open(ARCHIVE_PATH, mode="r:bz2")
 
-member_names = tfile.getnames()
-members = tfile.getmembers()
+# names = tfile.getnames()
+# members = tfile.getmembers()
 
-tgt_folders = [osp.join("datasets-master/training_datasets/LL0")]
+# pickle.dump(members, open(osp.expanduser("~/store/d3m/list_members.pkl"), "wb"))
+# pickle.dump(names, open(osp.expanduser("~/store/d3m/list_names.pkl"), "wb"))
+sizes_by_ext = {}
+count_by_ext = {}
+count = 0
+next_member = tfile.next()
 
-
-exts = []
-for folder in tgt_folders:
-    for mname in member_names:
-        if mname.startswith(folder):
-            name, ext = osp.splitext(mname)
-            exts.append(ext)
+tgt_ext = [".txt", ".csv"]
+progress = tqdm()
+# while next_member is not None:
+while next_member is not None:
+    if next_member.isreg():
+        name, ext = osp.splitext(next_member.name.strip())
+        if ext in sizes_by_ext:
+            sizes_by_ext[ext] += next_member.size
+        else:
+            sizes_by_ext[ext] = next_member.size
             
-ext_counts = Counter(exts)
+        if ext in count_by_ext:
+            count_by_ext[ext] += 1
+        else:
+            count_by_ext[ext] = 1
+
+        if ext in tgt_ext:
+            tfile.extract(next_member.name, osp.expanduser("~/store/d3m/extracted_files"))
+
+    next_member = tfile.next()
+    count += 1
+    progress.update()
+    
+pickle.dump(sizes_by_ext, open(osp.expanduser("~/store/d3m/sizes_by_ext.pkl"), "wb"))
+pickle.dump(count_by_ext, open(osp.expanduser("~/store/d3m/count_by_ext.pkl"), "wb"))
