2ae629c8c7535461176c3834a2190b3dd7719021
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-01-26 19:20:59 +0100

    Refactored script.

--- query_auctus.py
+++ query_auctus.py
@@ -8,33 +8,42 @@ import json
 from tqdm import tqdm
 import pandas as pd
 
-VALID_PATH = Path("small_set.txt")
+def reading_dataset_paths(VALID_PATH):
+    valid_paths = []
+    with open(VALID_PATH, "r") as fp:
+        n_paths = int(fp.readline().strip())
+        for idx, row in enumerate(fp):
+            valid_paths.append(Path(row.strip()))
+    return valid_paths
 
 
-valid_paths = []
-with open(VALID_PATH, "r") as fp:
-    n_paths = int(fp.readline().strip())
-    for idx, row in enumerate(fp):
-        valid_paths.append(Path(row.strip()))
+if __name__ == "__main__":
+    VALID_PATH = Path("small_set.txt")
+    query_limit = 100 # Maximum number of candidates to return when querying with a dataset
+    query_timeout = None # Number of seconds to wait before timeout, this might become useful later. 
 
-failed_datasets = []
-# Number of candidates (unfiltered) for successful datasets
-ok_datasets_candidates = {}
-client = datamart_rest.RESTDatamart('https://auctus.vida-nyu.org/api/v1')
-    
-for v_path in tqdm(valid_paths):
-    ds_name = v_path.stem
-    target_dataset_learning_data = v_path/f"{ds_name}_dataset"/Path("tables/learningData.csv")
-    assert target_dataset_learning_data.exists()
-    df = pd.read_csv(target_dataset_learning_data)
+    valid_paths = reading_dataset_paths(VALID_PATH)
 
-    full_container = container.Dataset.load(target_dataset_learning_data.absolute().as_uri()) 
-    
-    try:
-        cursor = client.search_with_data(query={}, supplied_data=full_container)
-        results = cursor.get_next_page(limit=100, timeout=None)
-        ok_datasets_candidates[ds_name] = len(results)
-    except Exception as e:    
-        print(f"Server error for {v_path}")
-        failed_datasets.append(v_path)
-    
+    failed_datasets = []
+    # Number of candidates (unfiltered) for successful datasets
+    results_by_dataset = {}
+
+    # Connecting to the API
+    client = datamart_rest.RESTDatamart('https://auctus.vida-nyu.org/api/v1')
+
+        
+    for v_path in tqdm(valid_paths):
+        ds_name = v_path.stem
+        target_dataset_learning_data = v_path/f"{ds_name}_dataset"/Path("tables/learningData.csv")
+        assert target_dataset_learning_data.exists()
+
+        # Loading the D3M representation
+        full_container = container.Dataset.load(target_dataset_learning_data.absolute().as_uri()) 
+        
+        try:
+            cursor = client.search_with_data(query={}, supplied_data=full_container)
+            results = cursor.get_next_page(limit=100, timeout=None)
+            results_by_dataset[ds_name] = results
+        except Exception as e:    
+            print(f"Server error for {v_path}")
+            failed_datasets.append(v_path)
