ec6669d461c293797bf8fdb49b2ebf349dcf7f73
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-02-08 18:53:30 +0100

    Added check for for datasets with no splits.

--- wrangle_metadata.py
+++ wrangle_metadata.py
@@ -38,7 +38,7 @@ info_fields.update(zip(possible_dtypes,[0 for _ in possible_dtypes]))
 
 overall_stats_dict = {}
 list_viable_datasets = []
-
+list_viable_with_splits = []
 
 folders = glob.glob(f"{DATA_PATH}/training_datasets/*/*") + \
     glob.glob(f"{DATA_PATH}/seed_datasets_current/*")
@@ -50,16 +50,21 @@ for folder in folders:
     json_dataset = json.load(open(
         osp.join(folder, dataset_folder, "datasetDoc.json")))
     dsetID, info_ = extract_from_json(json_dataset)
-    if info_[dsetID]["tabular_only"] and osp.exists(osp.join(folder,problem_folder, "dataSplits.csv")):
+    if info_[dsetID]["tabular_only"]:
         list_viable_datasets.append(folder) 
+        if osp.exists(osp.join(folder,problem_folder, "dataSplits.csv")):
+            list_viable_with_splits.append(folder)
     overall_stats_dict.update(info_)
+
 
 df_stats = pd.DataFrame().from_dict(overall_stats_dict, orient="index")
 print(df_stats.drop(["datasetName", "tot_res"], axis=1).sum())
 print(f'Dataset with the largest number of tables: {df_stats["table"].max()}')
 print(f"Number of viable dataset: {len(list_viable_datasets)}")
+print(f"Number of viable dataset with splits: {len(list_viable_with_splits)}")
 
 with open("viable_datasets.txt", "w") as fp:
-    fp.write(f"{len(list_viable_datasets)}\n")
-    for ds in list_viable_datasets:
+    # Saving only viable with splits to maintain consistency with previous behavior
+    fp.write(f"{len(list_viable_with_splits)}\n")
+    for ds in list_viable_with_splits:
         fp.write(f"{ds}\n")
