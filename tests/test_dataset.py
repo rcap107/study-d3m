216094bdc266188ae602436eb43c370872296d70
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-02-14 14:57:02 +0100

    Tests (?) for dataset class.

---
+++ tests/test_dataset.py
@@ -0,0 +1,14 @@
+import unittest
+import src.dataset_object as dso
+
+class DatasetTestCase(unittest.TestCase):
+    
+    
+    def test_base_dataset(self):
+        pass
+    
+    def test_d3m_dataset(self):
+        pass
+    
+    def test_gittables_dataset(self):
+        pass
